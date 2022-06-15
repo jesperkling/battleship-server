1b1623ab97a2cf1d15017145578f7f2ba39fe8be
Jesper Kling <63609498+jesperkling@users.noreply.github.com>
2022-06-15 22:27:49 +0200

    Handling incoming username works.

--- controllers/socket_controller.js
+++ controllers/socket_controller.js
@@ -5,6 +5,30 @@
 const debug = require('debug')('battleships:socket_controller');
 let io = null; // socket.io server instance
 
+const players = []
+
+/**
+ * Handle a user connecting
+ * 
+ */
+const handleConnect = function(username) {
+	debug(`${username} connected with id ${this.id} wants to join game`)
+
+	const player = {
+		id: this.id,
+		username: username,
+		ready: false,
+		ships: {},
+	}
+
+	players[this.id] = username
+	players.push(player)
+
+	console.log(player.username)
+
+	this.broadcast.emit('username', player.username)
+}
+
 /**
  * Handle a user disconnecting
  *
@@ -13,38 +37,38 @@ const handleDisconnect = function() {
 	debug(`Client ${this.id} disconnected :(`);
 }
 
-/**
- * Handle clock start
- *
- */
-const handleClockStart = function() {
-	debug(`Client ${this.id} wants to start the clock`);
+// /**
+//  * Handle clock start
+//  *
+//  */
+// const handleClockStart = function() {
+// 	debug(`Client ${this.id} wants to start the clock`);
 
-	// tell everyone connected to start their clocks
-	io.emit('clock:start')
-}
+// 	// tell everyone connected to start their clocks
+// 	io.emit('clock:start')
+// }
 
-/**
- * Handle clock stop
- *
- */
-const handleClockStop = function() {
-	debug(`Client ${this.id} wants to stop the clock`);
+// /**
+//  * Handle clock stop
+//  *
+//  */
+// const handleClockStop = function() {
+// 	debug(`Client ${this.id} wants to stop the clock`);
 
-	// tell everyone connected to stop their clocks
-	io.emit('clock:stop')
-}
+// 	// tell everyone connected to stop their clocks
+// 	io.emit('clock:stop')
+// }
 
-/**
- * Handle clock reset
- *
- */
-const handleClockReset = function() {
-	debug(`Client ${this.id} wants to reset the clock`);
+// /**
+//  * Handle clock reset
+//  *
+//  */
+// const handleClockReset = function() {
+// 	debug(`Client ${this.id} wants to reset the clock`);
 
-	// tell everyone connected to reset their clocks
-	io.emit('clock:reset')
-}
+// 	// tell everyone connected to reset their clocks
+// 	io.emit('clock:reset')
+// }
 
 /**
  * Export controller and attach handlers to events
@@ -55,16 +79,17 @@ module.exports = function(socket, _io) {
 	io = _io;
 
 	debug(`Client ${socket.id} connected`)
+	socket.on('player:username', handleConnect)
 
 	// handle user disconnect
 	socket.on('disconnect', handleDisconnect);
 
-	// listen for 'clock:start' event
-	socket.on('clock:start', handleClockStart)
+	// // listen for 'clock:start' event
+	// socket.on('clock:start', handleClockStart)
 
-	// listen for 'clock:stop' event
-	socket.on('clock:stop', handleClockStop)
+	// // listen for 'clock:stop' event
+	// socket.on('clock:stop', handleClockStop)
 
-	// listen for 'clock:reset' event
-	socket.on('clock:reset', handleClockReset)
+	// // listen for 'clock:reset' event
+	// socket.on('clock:reset', handleClockReset)
 }
